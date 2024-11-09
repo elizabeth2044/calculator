375b078092f054204d89fca3623c431defc9e260
Daniel J <d4niel.djm@gmail.com>
2024-11-09 16:36:09 -0500

    remove unnecessary code

--- main.ts
+++ main.ts
@@ -18,29 +18,3 @@ async function read(command: string) {
   command = command.split("\n")[0].trim();
   return command;
 }
-
-async function fetchData(urls: string[]) {
-  const promiseOfOsitos = fetch("https://www.google.com/search?q=ositos"); // busqueda de ositos
-  const promiseOfPerritos = fetch("https://www.google.com/search?q=perritos"); // busqueda de perritos
-  const promiseOfLoritos = fetch("https://www.google.com/search?q=loritos"); // busqueda de loritos
-
-  const allResponses = await Promise.all([
-    promiseOfOsitos,
-    promiseOfPerritos,
-    promiseOfLoritos,
-  ]);
-
-  const ositos = allResponses[0];
-  const perritos = allResponses[1];
-  const loritos = allResponses[2];
-}
-
-const currentUser = async () => {
-  return "123";
-};
-
-const fetchBills = async () => {
-  const userId = await currentUser();
-  const bills = await fetch(`https://myapi.com/bills/${userId}`);
-  return bills;
-};
